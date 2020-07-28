a469f2a914f08100ccd57c3d3f346a53d0e6fee1
jocelyntjahyadi <jocelyntjahyadi26@gmail.com>
2020-07-28 11:58:26 +0700

    add virtual account view

---
+++ app/src/main/java/com/example/tsaving/AddVaActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tsaving
+
+import android.app.Activity
+import android.os.Bundle
+
+class AddVaActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_va)
+    }
+
+}
+
