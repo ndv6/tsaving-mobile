9719e7405a1c333a95181fcc1869264a8e8a7e41
Caesar Pamungkas <caesargusti@gmail.com>
2020-07-28 11:29:19 +0700

    TSaving First Attemp Login and Register

---
+++ app/src/main/java/com/example/tsaving/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.tsaving
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        txt_register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
