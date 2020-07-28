9a28eef69c3147e173fc7824eaa0dc3a50ef7285
ayusrina <ayusrinaa@gmail.com>
2020-07-28 13:32:29 +0700

    feature update password

---
+++ app/src/main/java/com/example/tsaving/UpdatePassword.kt
@@ -0,0 +1,11 @@
+package com.example.tsaving
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdatePassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_password)
+    }
+}
