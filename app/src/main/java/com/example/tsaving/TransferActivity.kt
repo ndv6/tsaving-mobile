c53b4668c0fa3a790c3f8a31b5b4ef996c08ef65
David Bernadi <davidbernadi13@gmail.com>
2020-07-28 16:54:42 +0700

    fixing name convection

--- app/src/main/java/com/example/tsaving/TransferActivity.kt
+++ app/src/main/java/com/example/tsaving/TransferActivity.kt
@@ -2,30 +2,42 @@ package com.example.tsaving
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_transfer.*
+import java.lang.StringBuilder
+import java.text.NumberFormat
 
 class TransferActivity: Activity() {
+    lateinit var et_amount: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer)
-        txt_tf_from.setText("  "+ "TRANSFER FROM")
-        txt_tf_to.setText("  "+ "TRANSFER TO")
-        txt_tf_information.setText("  "+ "TRANSFER INFORMATION")
-
-
+        et_amount = findViewById(R.id.et_tf_amount_input)
+        et_amount.addTextChangedListener(textWatcher)
         //logic for submit transfer
         btn_tf_transfer.setOnClickListener {
-            val txtAmount = txt_tf_amount_input.text.toString()
-            if(txtAmount.isBlank()){
+            val amount = et_tf_amount_input.text.toString()
+            if(amount.isBlank()){
                 Toast.makeText(applicationContext,"Please Input The Amount", Toast.LENGTH_SHORT).show()
             } else{
                 Toast.makeText(applicationContext,"Good To Go", Toast.LENGTH_SHORT).show()
             }
         }
-
         btn_tf_back.setOnClickListener {
             finish()
+        }
+    } //end override
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+        }
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            //will implemented soon
         }
     }
 
