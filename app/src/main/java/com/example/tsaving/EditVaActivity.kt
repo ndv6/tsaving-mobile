3e2cf582ca79e560c4893cd71c1d9d5983c7ff3d
sekar minati <sekar.minati@gmail.com>
2020-07-29 13:40:01 +0700

    validation edit va

--- app/src/main/java/com/example/tsaving/EditVaActivity.kt
+++ app/src/main/java/com/example/tsaving/EditVaActivity.kt
@@ -3,15 +3,30 @@ package com.example.tsaving
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import com.example.tsaving.vm.EditVaViewModel
 import kotlinx.android.synthetic.main.activity_va_edit.*
 
-class EditVaActivity: Activity() {
+class EditVaActivity: AppCompatActivity(), LifecycleOwner {
+
+    private val editVaViewModel : EditVaViewModel = EditVaViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_va_edit)
+        lifecycle.addObserver(editVaViewModel)
 
         btn_vae_save.setOnClickListener {
-            startActivity(Intent(this, VADetailsActivity::class.java))
+            val label = et_vae_label.text.toString()
+            val status = editVaViewModel.validateEditVa(label)
+            if (!status) {
+                Toast.makeText(applicationContext, "Please input data", Toast.LENGTH_SHORT).show()
+            } else {
+                startActivity(Intent(this, VADetailsActivity::class.java))
+            }
         }
     }
 }
