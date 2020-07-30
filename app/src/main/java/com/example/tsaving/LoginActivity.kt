79062a91bfd604ea9d58de2f902df0b0af4f242b
David Bernadi <davidbernadi13@gmail.com>
2020-07-30 13:22:52 +0700

    wip local #2

--- app/src/main/java/com/example/tsaving/LoginActivity.kt
+++ app/src/main/java/com/example/tsaving/LoginActivity.kt
@@ -52,46 +52,46 @@ class LoginActivity : AppCompatActivity(), CoroutineScope, LifecycleOwner {
                         }
                     }
             })
-//            if(statusLogin){
-//                Toast.makeText(applicationContext, "Login Success", Toast.LENGTH_SHORT).show()
-//                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-//            }
+            if(statusLogin){
+                Toast.makeText(applicationContext, "Login Success", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+            }
 
 
-//            var repo: TsavingRepository = TsavingRepository()
-//
-//            //please change email & passwordnya from edit text then delete this comment
-//            var request: LoginRequestModel = LoginRequestModel("vc@gmail.com", "vcvcvc")
-//            Log.i("login req :", request.toString())
-//
-//            lifecycleScope.launch {
-//                try {
-//                    val result = withContext(Dispatchers.IO) { repo.login(request) }
-//                    Log.i("result", result.toString())
-//                } catch (t: Throwable) {
-//                    when (t) {
-//                        is IOException -> {
-//                            //Please change this to handle error with Sekar's dialog box then delete this comment
-//                            Toast.makeText(this@LoginActivity, "Network Error", Toast.LENGTH_SHORT)
-//                                .show()
-//                        }
-////                        is HttpException -> {
-////                            //Please change this to handle error with Sekar's dialog box then delete this comment
-////                            val code = t.code()
-////                            val errMsg = t.response().toString()
-////                            Log.i("login error message", t.response().toString())
-////                            Toast.makeText(
-////                                this@LoginActivity,
-////                                "httpError $code $errMsg",
-////                                Toast.LENGTH_SHORT
-////                            )
-////                                .show()
-////                        }
-//                    }
-//                }
-//            }
-//            // Here, please code what the app will do if API call succeed, then delete this comment
-//            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+            var repo: TsavingRepository = TsavingRepository()
+
+            //please change email & passwordnya from edit text then delete this comment
+            var request: LoginRequestModel = LoginRequestModel("vc@gmail.com", "vcvcvc")
+            Log.i("login req :", request.toString())
+
+            lifecycleScope.launch {
+                try {
+                    val result = withContext(Dispatchers.IO) { repo.login(request) }
+                    Log.i("result", result.toString())
+                } catch (t: Throwable) {
+                    when (t) {
+                        is IOException -> {
+                            //Please change this to handle error with Sekar's dialog box then delete this comment
+                            Toast.makeText(this@LoginActivity, "Network Error", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        is HttpException -> {
+                            //Please change this to handle error with Sekar's dialog box then delete this comment
+                            val code = t.code()
+                            val errMsg = t.response().toString()
+                            Log.i("login error message", t.response().toString())
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "httpError $code $errMsg",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        }
+                    }
+                }
+            }
+            // Here, please code what the app will do if API call succeed, then delete this comment
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
         } //end on login on click listener
     }
 }
