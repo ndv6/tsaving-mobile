12020e610256abd596c33d615d62622f3155f43c
sekar minati <sekar.minati@gmail.com>
2020-08-01 19:34:04 +0700

    resolve conflict

--- app/src/main/java/com/example/tsaving/vm/EditVaViewModel.kt
+++ app/src/main/java/com/example/tsaving/vm/EditVaViewModel.kt
@@ -1,8 +1,35 @@
 package com.example.tsaving.vm
 
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.*
+import com.example.tsaving.model.ResponseModel
+import com.example.tsaving.webservice.TsavingRepository
+import kotlinx.coroutines.*
+import retrofit2.HttpException
+import java.io.IOException
+import kotlin.coroutines.CoroutineContext
 
-class EditVaViewModel : ViewModel(), LifecycleObserver {
+class EditVaViewModel (private val tsRepo: TsavingRepository) : ViewModel(), CoroutineScope, LifecycleObserver {
+    private var job: Job = Job()
+    override val coroutineContext: CoroutineContext get() = job + Dispatchers.Main
+    private var _data = MutableLiveData<ResponseModel>()
+    var data : LiveData<ResponseModel> = _data
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    private fun fetchVaEditData(){
+        viewModelScope.launch {
+            try {
+                val result= withContext(Dispatchers.IO) {tsRepo.updateVa(vaNum)}
+                _data.value = result
+                Log.i("result", result.toString())
+            }catch (t: Throwable){
+                when (t) {
+                    is IOException -> println(t.message)
+                    is HttpException -> println(t.message)
+                }
+            }
+        }
+    }
+
     fun validateEditVa(label: String) :Boolean = !label.isBlank()
 }
