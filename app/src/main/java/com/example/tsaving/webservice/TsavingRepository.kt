463a6f2a0305ae36323c2f9302f94893491a7091
Joseph Marcel <polipopoliko@gmail.com>
2020-08-03 00:06:06 +0700

    Changing response model to generic type

--- app/src/main/java/com/example/tsaving/webservice/TsavingRepository.kt
+++ app/src/main/java/com/example/tsaving/webservice/TsavingRepository.kt
@@ -1,6 +1,7 @@
 package com.example.tsaving.webservice
 
 import com.example.tsaving.BaseApplication
+import com.example.tsaving.model.DashboardModel
 import com.example.tsaving.model.DashboardResponseModel
 import com.example.tsaving.model.request.*
 import com.example.tsaving.model.response.AddVaResponseModel
@@ -26,7 +27,7 @@ class TsavingRepository {
     suspend fun updatePassword(body: UpdatePasswordRequestModel) = webService.updatePassword(BaseApplication.token, body)
     suspend fun viewProfile() = webService.viewProfile(BaseApplication.token)
     suspend fun updateProfile(body: EditProfileRequestModel) = webService.updateProfile(BaseApplication.token, body)
-    suspend fun dashboard() : DashboardResponseModel = webService.dashboard(BaseApplication.token)
+    suspend fun dashboard() : GenericResponseModel<DashboardModel> = webService.dashboard(BaseApplication.token)
     suspend fun updatePhoto() = webService.updatePhoto()
     suspend fun listVa() = webService.listVa(BaseApplication.token)
     suspend fun createVa(body: AddVaRequestModel) : GenericResponseModel<Any> = webService.createVa(body,BaseApplication.token)
