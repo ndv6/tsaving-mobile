c0a56def88d26725afbe0dcd54a855e155c6174c
Joseph Marcel <polipopoliko@gmail.com>
2020-08-03 15:30:17 +0700

    Adding formatter and changing image to placeholder

--- app/src/main/java/com/example/tsaving/DashboardActivity.kt
+++ app/src/main/java/com/example/tsaving/DashboardActivity.kt
@@ -43,7 +43,7 @@ class DashboardFragment() : androidx.fragment.app.Fragment(), LifecycleOwner {
                 tv_dashboard_name.text = it.data.custName
                 tv_dashboard_email.text = it.data.custEmail
                 tv_dashboard_acc_num.text = it.data.accountNum
-                tv_dashboard_acc_balance.text = "Rp. ${it.data.accountBalance}"
+                tv_dashboard_acc_balance.text = "Rp. ${it.data.accountBalance.toString().FormatDecimal()}"
                 dashboardAdapter.vaList = it.data.virtualAccounts
                 dashboardAdapter.notifyDataSetChanged()
             })
