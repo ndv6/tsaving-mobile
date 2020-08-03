7e1bb426ee4a800b15a59be249ad9a84a72c6ed8
vicimilenia <vici.ocbcnisp@gmail.com>
2020-08-03 16:57:18 +0700

    fix time formatting

--- app/src/main/java/com/example/tsaving/helpers/TransactionHistoryHelper.kt
+++ app/src/main/java/com/example/tsaving/helpers/TransactionHistoryHelper.kt
@@ -8,8 +8,8 @@ import com.example.tsaving.R
 class TransactionHistoryHelper {
     companion object {
         private var DEPOSIT_MESSAGE = "Deposit from Partner"
-        private var MAIN_TO_VA = "Transfer to Virtual Account"
-        private var VA_TO_MAIN = "Transfer to Main Account"
+        private var MAIN_TO_VA_MESSAGE = "Transfer to Virtual Account"
+        private var VA_TO_MAIN_MESSAGE = "Transfer to Main Account"
 
         fun setImgSrc(desc: String, img: ImageView) {
             when (desc) {
@@ -27,18 +27,21 @@ class TransactionHistoryHelper {
             }
         }
 
-        fun formatTransactionHistoryTitle(transactionHistoryTextView: TextView){
-            when(transactionHistoryTextView.text) {
+        fun formatTransactionHistoryTitle(
+            transactionHistoryTextView: TextView,
+            description: String
+        ) {
+            when (description) {
                 "DEPOSIT_TO_MAIN_ACCOUNT" -> {
-
+                    transactionHistoryTextView.text = DEPOSIT_MESSAGE
                 }
 
                 "MAIN_TO_VA" -> {
-
+                    transactionHistoryTextView.text = MAIN_TO_VA_MESSAGE
                 }
 
                 "VA_TO_MAIN" -> {
-
+                    transactionHistoryTextView.text = VA_TO_MAIN_MESSAGE
                 }
             }
         }
