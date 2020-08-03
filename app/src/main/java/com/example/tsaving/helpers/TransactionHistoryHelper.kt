b147c5eabfaa44942bebaf13a2266f1361c86c11
vicimilenia <vici.ocbcnisp@gmail.com>
2020-08-03 15:40:35 +0700

    fix UI

--- app/src/main/java/com/example/tsaving/helpers/TransactionHistoryHelper.kt
+++ app/src/main/java/com/example/tsaving/helpers/TransactionHistoryHelper.kt
@@ -7,6 +7,10 @@ import com.example.tsaving.R
 
 class TransactionHistoryHelper {
     companion object {
+        private var DEPOSIT_MESSAGE = "Deposit from Partner"
+        private var MAIN_TO_VA = "Transfer to Virtual Account"
+        private var VA_TO_MAIN = "Transfer to Main Account"
+
         fun setImgSrc(desc: String, img: ImageView) {
             when (desc) {
                 "DEPOSIT_TO_MAIN_ACCOUNT" -> {
@@ -19,6 +23,22 @@ class TransactionHistoryHelper {
 
                 "VA_TO_MAIN" -> {
                     img.setImageResource(R.mipmap.ic_transaction_history_incoming_round)
+                }
+            }
+        }
+
+        fun formatTransactionHistoryTitle(transactionHistoryTextView: TextView){
+            when(transactionHistoryTextView.text) {
+                "DEPOSIT_TO_MAIN_ACCOUNT" -> {
+
+                }
+
+                "MAIN_TO_VA" -> {
+
+                }
+
+                "VA_TO_MAIN" -> {
+
                 }
             }
         }
